fef9fba36a38c1832168a2ecd964daa3dcbde2dc
Adrian A <51465995+MadcapMexican@users.noreply.github.com>
2023-12-18 10:08:29 -0800

    Cursor Changes
    Worked on the cursor controller to have a changing cursor on clicking. Uses the input mapping made and temporary textures.

--- Diner Feeding/Assets/Scripts/CursorController.cs
+++ Diner Feeding/Assets/Scripts/CursorController.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-    GameObject selectedObject = null;
+    public Texture2D cursor;
+    public Texture2D cursorClicked;
 
-    // Update is called once per frame
-    void Update()
+    private CursorControls controls;
+
+    private void Awake()
     {
-        
+        controls = new CursorControls(); //Instatiate new controls
+        //Setting the base cursor to the default texture
+        CursorChanged(cursor);
+        Cursor.lockState = CursorLockMode.Confined; //Lock cursor to game screen
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void Start()
+    {
+        controls.Mouse.Click.started += _ => StartedClick();
+        controls.Mouse.Click.performed += _ => EndedClick();
+    }
+
+    private void StartedClick() {
+        //Set clicked cursor when clicked
+        CursorChanged(cursorClicked);
+    }
+
+    private void EndedClick() {
+        //Change back to pointer when finished grabbing
+        CursorChanged(cursor);
+    }
+
+    //Function to change our cursor to the correct one according to action
+    public void CursorChanged(Texture2D cursorType) {
+        //Vector2 to check middle of cursor for selection instead of top left like a pointint one does
+        //Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
     }
 }
